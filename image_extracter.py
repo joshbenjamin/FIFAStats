a8502ab3d49c77215a47cad0baa6fcda09a46e3d
Joshua Benjamin <joshbenjamin13@gmail.com>
2020-09-12 23:04:44 +0200

    Cleaned up some code

--- image_extracter.py
+++ image_extracter.py
@@ -1,31 +1,23 @@
 from read_file import Position, Rating, read_file
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 from PIL import Image
-import numpy as np
 import math
 
 import typing
 
-from random import randint, seed
-
 
 def main():
-	seed(1)
 	positions_file = "ImagePositions.txt"
 	image_file = "C:\\Users\\Joshua\\Documents\\Development\\FIFAStats\\Screenshots\\FIFA 20 Seasons (In Menus)_6.jpg"
 	ratings = read_file(positions_file)
-	# extract_smaller_image(ratings[0].position_start, ratings[0].position_end, image_file, ratings)
 	crop_image(image_file, ratings)
 	print("Hello World")
 
 
-def crop_image(file: str, ratings):
+def crop_image(file: str, ratings: typing.List[Rating]) -> str:
 
 	im = Image.open(file)
 
-	images = []
 	for rat in ratings:
 		width_height = width_and_height(rat.position_start, rat.position_end)
 		im_crop = im.crop((rat.position_start.x,
@@ -35,16 +27,8 @@ def crop_image(file: str, ratings):
 
 		if analyse_block(im_crop):
 			print("THIS IS THE ONE: " + rat.title)
-			im_crop.show()
-		else:
-			print("Ain't it: " + rat.title)
-
-		# images.append(im_crop)
-	"""
-	for img in images:
-		# img.show()
-		analyse_block(img)
-	"""
+			# im_crop.show()
+			return rat.title
 
 
 def analyse_block(image: Image) -> bool:
