e80153da7911d42ba401cf6f570d32debc0bb684
Joshua Benjamin <joshbenjamin13@gmail.com>
2020-09-13 00:12:39 +0200

    Basic OCR done now but it doesn't really work at all

--- image_extracter.py
+++ image_extracter.py
@@ -1,7 +1,11 @@
 from read_file import Position, Rating, read_file
+from tesseract_functions import *
 
 from PIL import Image
 import math
+import cv2
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 
 import typing
 
@@ -18,13 +22,34 @@ def main():
 	crop_ratings_image(image)
 
 
+def tesseract_work(filename: str):
+	image = cv2.imread(filename)
+	gray = get_grayscale(image)
+	thresh = thresholding(gray)
+
+	h, w, c = thresh.shape
+	boxes = pytesseract.image_to_boxes(thresh)
+	for b in boxes.splitlines():
+		b = b.split(' ')
+		thresh = cv2.rectangle(thresh, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+
+	cv2.imshow('img', thresh)
+	cv2.waitKey(0)
+
+	custom_config = r'--oem 3 --psm 6'
+	print(pytesseract.image_to_string(thresh, config=custom_config))
+	# cv2.imshow('thresh', thresh)
+	# cv2.waitKey(0)
+
+
 def crop_ratings_image(image: Image):
 	width_height = width_and_height(START_POSITION_MAIN, END_POSITION_MAIN)
 	im_crop = image.crop((START_POSITION_MAIN.x,
 						  START_POSITION_MAIN.y,
 						  START_POSITION_MAIN.x + width_height[0],
 						  START_POSITION_MAIN.y + width_height[1]))
-	im_crop.show()
+	im_crop.save("temp.png", "PNG")
+	tesseract_work("temp.png")
 
 
 def rating_type(image: Image, ratings: typing.List[Rating]) -> str:
