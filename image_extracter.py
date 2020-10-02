2e3ce2c42487c0e0dddece69695aaafc99e9ef39
Joshua Benjamin <joshbenjamin13@gmail.com>
2020-10-02 23:18:25 +0200

    Basic OCR done very well on a single stat

--- image_extracter.py
+++ image_extracter.py
@@ -2,6 +2,7 @@ from read_file import Position, Score, read_file
 from tesseract_functions import *
 
 from PIL import Image
+import numpy
 import math
 import cv2
 import pytesseract
@@ -15,55 +16,63 @@ END_POSITION_MAIN = Position(1702, 808)
 
 def main():
 	positions_file = "Positions - Performance.txt"
-	image_file = "C:\\Users\\Joshua\\Documents\\Development\\FIFAStats\\Pictures - Performance\\FIFA 20 Seasons (In Menus)_6.jpg"
+	image_filepath = "C:\\Users\\Joshua\\Documents\\Development\\FIFAStats\\Pictures - Performance\\Mason.jpg"
 
-	image = Image.open(image_file)
+	image = Image.open(image_filepath)
 	scores = read_file(positions_file)
 
-	draw_rectangles(image, scores)
-
-	# crop_ratings_image(image)
+	indi_image(image, scores)
 
 
-def draw_rectangles(image: Image, scores: typing.List[Score]):
-	for score in scores:
+def analyse_image(type_char: str, image: Image):
+	cv2_image = numpy.array(image.convert('RGB'))
+	# cv2_image = cv2_image[:, :, ::-1].copy()           unused, to convert RGB-to-BGR
+	if type_char == "number":
+		tesseract_work(cv2_image)
+
+
+def indi_image(image: Image, scores: typing.List[Score]):
+	# for score in scores:
+
+	# for i in range(0, len(scores)):
+	for i in range(5, 6):
+		score = scores[i]
 		width_height = width_and_height(score.position_start, score.position_end)
 		im_crop = image.crop((score.position_start.x,
 							  score.position_start.y,
 							  score.position_start.x + width_height[0],
 							  score.position_start.y + width_height[1]))
-		im_crop.show(score.title, im_crop)
+		# im_crop.show(score.title, im_crop)
+		# im_crop.save("temp.png", "PNG")
+		analyse_image("number", im_crop)
 
 
-def tesseract_work(filename: str):
-	image = cv2.imread(filename)
-	gray = get_grayscale(image)
-	thresh = thresholding(gray)
+def tesseract_work(image: numpy.ndarray):
+	# image = cv2.imread(filename)
+	# gray = get_grayscale(image)
+	# thresh = thresholding(image)
 
+	"""
 	h, w, c = thresh.shape
 	boxes = pytesseract.image_to_boxes(thresh)
 	for b in boxes.splitlines():
 		b = b.split(' ')
 		thresh = cv2.rectangle(thresh, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
-
-	cv2.imshow('img', thresh)
+	"""
+	print(type(image))
+	cv2.imshow('img', image)
 	cv2.waitKey(0)
 
 	custom_config = r'--oem 3 --psm 6'
-	print(pytesseract.image_to_string(thresh, config=custom_config))
+	image_text = pytesseract.image_to_string(image, config=custom_config)
+	image_text = strip_arrow(image_text)
+	# print(image_text)
 	# cv2.imshow('thresh', thresh)
 	# cv2.waitKey(0)
 
-
-def crop_ratings_image(image: Image):
-	width_height = width_and_height(START_POSITION_MAIN, END_POSITION_MAIN)
-	im_crop = image.crop((START_POSITION_MAIN.x,
-						  START_POSITION_MAIN.y,
-						  START_POSITION_MAIN.x + width_height[0],
-						  START_POSITION_MAIN.y + width_height[1]))
-	im_crop.save("temp.png", "PNG")
-	tesseract_work("temp.png")
-
+def strip_arrow(text: str):
+	print(text)
+	return text
 
 def rating_type(image: Image, scores: typing.List[Score]) -> str:
 
